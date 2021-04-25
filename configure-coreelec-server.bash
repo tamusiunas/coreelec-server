3c1c6fb4b0a62ca2013cbe7fac08a5c3c28d6e2f
Fabricio Tamusiunas <51459553+fabriciotamusiunas@users.noreply.github.com>
2021-04-25 02:10:53 +0100

    opkg PATH

--- configure-coreelec-server.bash
+++ configure-coreelec-server.bash
@@ -21,7 +21,7 @@ echo ""
 echo "Installing Entware"
 echo ""
 echo "n" | /usr/sbin/installentware 
-opkg install gcc busybox ldd make gawk sed python3-pip patch diffutils coreutils-install git tar file
+/opt/bin/opkg install gcc busybox ldd make gawk sed python3-pip patch diffutils coreutils-install git tar file
 #
 # Install headers
 #
